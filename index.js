37754f042745bd3f9b251a192150823bceed257d
xITSDUCKYx <82958945+xITSDUCKYx@users.noreply.github.com>
2023-05-11 16:46:26 -0400

    End Game Modal v2
    Added New end game modal HTML using dialog.
    
    Added New end game modal CSS.
    
    Added JavaScript to control new end game modal.
    
    Added button UI advancements, hover translate etc.
    
    Removed duplicate original end modal.
    
    Removed galaxy-grid.png.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Connected!");
+const endGame = document.getElementById("end-game")
+const newGame = document.getElementById("end-game-new-game-btn")
+const endGameModal = document.getElementById("end-game-modal")
+document.addEventListener("click", function(e){
+    console.log(e.target)
+    if(e.target === endGame){
+        endGameModal.showModal()
+    } else if (e.target ===newGame){
+        endGameModal.close()
+    }
+
+})
+
+
+
