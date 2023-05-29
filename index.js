70729b786752e2802fd998e2c4f7ff0afe408dd4
xITSDUCKYx <82958945+xITSDUCKYx@users.noreply.github.com>
2023-05-29 14:01:20 -0400

    added diceElement

--- index.js
+++ index.js
@@ -1,46 +1,47 @@
+/* ---------- Start Game Modal ---------- */
+
+const modal = document.getElementById('startModal');
+
+window.onload = function displayModal() {
+  modal.classList.add('show-modal');
+};
+
+function closeModal() {
+  const inputElement = document.getElementById('playername');
+  const inputValue = inputElement.value;
+  const name = inputValue;
+
+  document.getElementById('name').innerText = `${name}`;
+  modal.classList.remove('show-modal');
+}
+
 /* ------------ End Game Modal ----------- */
 
-const endGame = document.getElementById("end-game");
-const newGame = document.getElementById("end-game-new-game-btn");
-const endGameModal = document.getElementById("end-game-modal");
-document.addEventListener("click", function (e) {
+const endGame = document.getElementById('end-game');
+const newGame = document.getElementById('end-game-new-game-btn');
+const endGameModal = document.getElementById('end-game-modal');
+document.addEventListener('click', function (e) {
   // console.log(e.target)
   if (e.target === endGame) {
     endGameModal.showModal();
   } else if (e.target === newGame) {
+    clearBoard();
     endGameModal.close();
   }
 });
 
-/* ---------- Start Game Modal ---------- */
-
-const modal = document.getElementById("startModal");
-
-window.onload = function displayModal() {
-  modal.classList.add("show-modal");
-};
-
-function closeModal() {
-  const inputElement = document.getElementById("playername");
-  const inputValue = inputElement.value;
-  const name = inputValue;
-
-  document.getElementById("name").innerText = `${name}`;
-  modal.classList.remove("show-modal");
-}
-
 /*------------- Grid ----------------*/
 
 function createBoard() {
-  const box = "";
+  const box = '';
   for (let a = 0; a < 10; a++) {
-    var row = document.createElement("div");
-    document.getElementById("grid-container").appendChild(row);
-    row.className = "row";
+    var row = document.createElement('div');
+    document.getElementById('grid-container').appendChild(row);
+    row.className = 'row';
     for (let b = 0; b < 10; b++) {
-      var column = document.createElement("div");
+      var column = document.createElement('div');
       column.innerHTML = box;
-      column.className = "column";
+      column.className = 'column';
       row.appendChild(column);
     }
   }
@@ -50,21 +51,39 @@ createBoard();
 
 /*---------- Coloring the Boxes --------------*/
 
-let gridBox = document.querySelectorAll(".column");
+let gridBox = document.querySelectorAll('.column');
 
-gridBox.forEach(function (gridBox){
-  let isGrey = true;
-  gridBox.addEventListener("click", () => {
-    if (isGrey) {
-      gridBox.style.backgroundColor = 'red';
-      isGrey = false;
-    } else {
-      gridBox.style.backgroundColor = 'grey';
-      isGrey = true;
+gridBox.forEach(function (gridBox) {
+  gridBox.addEventListener('mousedown', () => {
+    let currentColor = gridBox.style.backgroundColor;
+    
+    if (currentColor === "gray" || currentColor === "") {
+        gridBox.style.backgroundColor = 'aqua';
+      } else {
+      gridBox.style.backgroundColor = 'gray';
     }
+  });
+});
+
+/*-----------Clearing the Grid------------*/
+function clearBoard() {
+  const squares = document.querySelectorAll('.column');
   
+  const diceElements = ["dice1", "dice2"];
+  
+  squares.forEach((column) => {
+    column.style.backgroundColor = 'gray';
+  });
+
+  diceElements.forEach(function(elementId){
+    const diceElement = document.getElementById(elementId);
+    diceElement.innerText = "";
+    diceElement.value = 0;
   })
-})
+
+
+ 
+}
 
 /*-----------Dice roll------------- */
 function rollTheDice() {
@@ -72,12 +91,8 @@ function rollTheDice() {
   var d1 = Math.floor(Math.random() * 6) + 1;
   var d2 = Math.floor(Math.random() * 6) + 1;
 
-  //Prints dice rolls to console
-  console.log(d1);
-  console.log(d2);
-
-  var diceRoll1 = document.getElementById("dice1");
-  var diceRoll2 = document.getElementById("dice2");
+  var diceRoll1 = document.getElementById('dice1');
+  var diceRoll2 = document.getElementById('dice2');
 
   diceRoll1.innerText = d1;
   diceRoll2.innerText = d2;
