e1676e744cd2998b3e4237e45a42987e6f78c014
Tyler Thietje <tylerthietje@gmail.com>
2023-06-10 14:40:49 -0500

    Added functionality to trigger the alert modal if squares selected dont match the numbers rolled. And functionality to close the modal when the button is clicked

--- index.js
+++ index.js
@@ -1,44 +1,41 @@
 /* ---------- Global Variables ---------- */
-const confirmBtn = document.querySelector("confirm-btn");
-const cancelBtn = document.querySelector("cancel-btn");
-
-
+const confirmBtn = document.querySelector('confirm-btn');
+const cancelBtn = document.querySelector('cancel-btn');
 
 /* ---------- Start Game Modal ---------- */
-const modal = document.getElementById("startModal");
+const modal = document.getElementById('startModal');
 
 window.onload = function displayModal() {
-  modal.classList.add("show-modal");
+  modal.classList.add('show-modal');
 };
 
 function closeModal() {
-  const inputElement = document.getElementById("playername");
+  const inputElement = document.getElementById('playername');
   const inputValue = inputElement.value;
   const name = inputValue;
 
-  document.getElementById("name").innerText = `${name}`;
-  modal.classList.remove("show-modal");
+  document.getElementById('name').innerText = `${name}`;
+  modal.classList.remove('show-modal');
 }
 
 /* ---------- New Game Alert Modal ---------- */
 
-const alertModal = document.getElementById("newGameAlertModal");
+const alertModal = document.getElementById('newGameAlertModal');
 
 function displayAlertModal() {
-  alertModal.classList.add("show-modal");
-};
-
-function closeAlertModal() {  
-  alertModal.classList.remove("show-modal");
+  alertModal.classList.add('show-modal');
 }
 
+function closeAlertModal() {
+  alertModal.classList.remove('show-modal');
+}
 
 /* ------------ End Game Modal ----------- */
 
-const endGame = document.getElementById("end-game");
-const newGame = document.getElementById("end-game-new-game-btn");
-const endGameModal = document.getElementById("end-game-modal");
-document.addEventListener("click", function (e) {
+const endGame = document.getElementById('end-game');
+const newGame = document.getElementById('end-game-new-game-btn');
+const endGameModal = document.getElementById('end-game-modal');
+document.addEventListener('click', function (e) {
   // console.log(e.target)
   if (e.target === endGame) {
     endGameModal.showModal();
@@ -52,15 +49,15 @@ document.addEventListener("click", function (e) {
 /*------------- Grid ----------------*/
 
 function createBoard() {
-  const box = "";
+  const box = '';
   for (let a = 0; a < 10; a++) {
-    var row = document.createElement("div");
-    document.getElementById("grid-container").appendChild(row);
-    row.className = "row";
+    var row = document.createElement('div');
+    document.getElementById('grid-container').appendChild(row);
+    row.className = 'row';
     for (let b = 0; b < 10; b++) {
-      var column = document.createElement("div");
+      var column = document.createElement('div');
       column.innerHTML = box;
-      column.className = "column";
+      column.className = 'column';
       row.appendChild(column);
     }
   }
@@ -68,16 +65,16 @@ function createBoard() {
 
 createBoard();
 /*---------- Updates the leaderboard --------------*/
-const winDisplayEl = document.getElementById("scoreOne")
-const lossDisplayEl = document.getElementById("scoreTwo")
+const winDisplayEl = document.getElementById('scoreOne');
+const lossDisplayEl = document.getElementById('scoreTwo');
 let isWinner = false;
 let wins = 0;
 let losses = 0;
 
-function updateLearderboard(){
-  isWinner ? wins++ : losses++
-  winDisplayEl.textContent = wins
-  lossDisplayEl.textContent = losses
+function updateLearderboard() {
+  isWinner ? wins++ : losses++;
+  winDisplayEl.textContent = wins;
+  lossDisplayEl.textContent = losses;
 }
 
 /*---------- Coloring the Boxes --------------*/
@@ -85,71 +82,67 @@ let gridBox = document.querySelectorAll('.column');
 let diceBtn = document.getElementById('dice');
 var aquaCount = 0;
 
-
 gridBox.forEach(function (gridBox) {
-  gridBox.addEventListener("mousedown", () => {
+  gridBox.addEventListener('mousedown', () => {
     let currentColor = gridBox.style.backgroundColor;
 
-    if (currentColor === "gray" || currentColor === "") {
+    if (currentColor === 'gray' || currentColor === '') {
       gridBox.style.backgroundColor = 'aqua';
       aquaCount += 1;
       console.log(aquaCount);
     } else {
-      gridBox.style.backgroundColor = "gray";
+      gridBox.style.backgroundColor = 'gray';
     }
   });
-  
 });
 
-/*-----------Clearing the Grid------------
-function clearBoard() {
-  const squares = document.querySelectorAll('.column');
-  squares.forEach((column) => {
-    column.style.backgroundColor = 'gray';
-  });
-  
-}
-*/
 /*----------- Dice roll ------------- */
 var total = 0;
 function rollTheDice() {
   //Initializes dice roll
   var d1 = Math.floor(Math.random() * 6) + 1;
   var d2 = Math.floor(Math.random() * 6) + 1;
-  total = d1*d2;
+  total = d1 * d2;
 
-  var diceRoll1 = document.getElementById("dice1");
-  var diceRoll2 = document.getElementById("dice2");
+  var diceRoll1 = document.getElementById('dice1');
+  var diceRoll2 = document.getElementById('dice2');
 
   diceRoll1.innerText = d1;
   diceRoll2.innerText = d2;
-  console.log(total);
   diceBtn.disabled = true;
   return total;
 }
 
-var errorModal = document.getElementById("error-modal");
-function submitHandler () {
-    if(aquaCount < total){
-      errorModal.style.display = "block";
-    }else{
-      diceBtn.disabled = false;
-    }
-   
+/*----------- Squares Error Message Modal ------------*/
+
+var errorModal = document.getElementById('error-modal');
+
+function submitHandler() {
+  if (aquaCount !== total) {
+    // errorModal.style.display = "block";
+    errorModal.classList.add('show-modal');
+  } else {
+    diceBtn.disabled = false;
+    aquaCount = 0;
   }
+}
+
+function closeErrorModal() {
+  errorModal.classList.remove('show-modal');
+}
 
 /*----------- Clearing the Grid ------------*/
 function clearBoard() {
-  const squares = document.querySelectorAll(".column");
+  const squares = document.querySelectorAll('.column');
   squares.forEach((column) => {
-    column.style.backgroundColor = "gray";
+    column.style.backgroundColor = 'gray';
   });
   diceBtn.disabled = false;
+  aquaCount = 0;
 }
 
 /*----------- Clearing the Dice ------------*/
 function clearDiceBoard() {
-  document.getElementById("dice1").textContent = "";
-  document.getElementById("dice2").textContent = "";
+  document.getElementById('dice1').textContent = '';
+  document.getElementById('dice2').textContent = '';
 }
-
